0d140c40dd948684b81d64fcd36487927237e51c
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-11-12 14:49:52 +0100

    ADD: Show small thumb

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
 /*
  * Vue Slider
  */
+const root = new Vue({
+	el: "#root",
+	data: {
+		slides: [
+			{
+				image: "img/01.jpg",
+				title: "Svezia",
+				text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque providen totam omnis, magnam dolores dolorum corporis.",
+			},
+			{
+				image: "img/02.jpg",
+				title: "Svizzera",
+				text: "Lorem ipsum.",
+			},
+			{
+				image: "img/03.jpg",
+				title: "Gran Bretagna",
+				text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+			},
+			{
+				image: "img/04.jpg",
+				title: "Germania",
+				text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.",
+			},
+			{
+				image: "img/05.jpg",
+				title: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.",
+			},
+		],
+		actived: 0,
+	},
+	methods: {
+		getActive(thumbIndex, currentIndex) {
+			return thumbIndex === currentIndex ? `active` : "";
+		},
+	},
+});
